3ab9f1c779cbb7c312f14831b21a6e9e1cdfccc1
Xie <kurovoid.natalie@gmail.com>
2025-08-19 03:12:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const formatReadme = (data) => {
   const formattedReadme = []
   formattedReadme.push('# TradingView BTCUSD Scanner (1H)')
   formattedReadme.push('![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/rainbow.png)')
-  formattedReadme.push('## Last updated: ' + formattedDate)
+  formattedReadme.push('## Last updated: ' + formattedDate + '(GMT+7)')
   formattedReadme.push('## Price Now: ' + usdFormatter.format(data['close|60']))
   formattedReadme.push('![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/rainbow.png)')
   formattedReadme.push('<h2 id="about-the-project"> :pencil: About Project</h2>')
